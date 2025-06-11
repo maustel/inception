7563c3e3f23dd14d02da19ad9224f15b5e0364ff
maustel <milena-austel@gmx.de>
2025-06-11 13:26:13 +0200

    wordpress seems to work

--- testing/wordpress/tools/wp-setup.sh
+++ testing/wordpress/tools/wp-setup.sh
@@ -2,85 +2,106 @@
 
 #https://www.linode.com/docs/guides/how-to-install-wordpress-using-wp-cli-on-debian-10/#download-and-configure-wordpress
 
-echo "[WP-SETUP-SCRIPT]"
+YEL='\033[0;33m'
+BLUE='\[\033[0;34m\]'
+RESET='\033[0m'
+
+echo -e "${YEL}[WP-SETUP-SCRIPT]${RESET}"
 
 #print all commands in terminal
 set -x
+
+
+# -------------------[Wait for database] -----------
+echo -e "${BLUE}[***Waiting for mariadb to be ready.***]${RESET}"
+until mysqladmin ping -h mariadb --silent; do
+    sleep 2
+done
 
 #------------------ [prerequisites] -----------------
 #create necessary directories and give permissions
 mkdir -p /run/php
 chown www-data:www-data /run/php
 
-# # giving permission for php config file, overwriting existingphp config file with new config for listening on 0.0.0.0:9000
-# chmod +x /www.conf
-# mv -f www.conf /etc/php/7.4/fpm/pool.d/
-
+# giving permission for php config file, overwriting existingphp config file with new config for listening on 0.0.0.0:9000
+chmod +x /etc/php/7.4/fpm/pool.d/www.conf
+chown -R www-data:www-data /etc/php/7.4/fpm/pool.d/www.conf
 
 #install WP_CLI
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-#-------------------[Configure WordPress Dixectories] -----------
+#-------------------[Configure WordPress Directories] -----------
 
 #create wp site's document root (for nginx)
-mkdir -p /var/www/html/maustel.42.fr/public_html
+mkdir -p ${WP_PATH}
 #todo: put path in env
 
 #change ownership to www-data user and group, because nginx runs with www-data user
-chown -R www-data:www-data /var/www/html/maustel.42.fr/public_html
+chown -R www-data:www-data ${WP_PATH}
 
 # DAVID: give permission for cache (solved all my issues)
 mkdir -p /var/www/.wp-cli/cache
 chown -R www-data:www-data /var/www/.wp-cli
 
-cd /var/www/html/maustel.42.fr/public_html
+cd ${WP_PATH}
 
 #-------------------[Download and Configure WordPress] -----------
 
+if wp core is-installed --allow-root > /dev/null 2>&1; then
+	echo -e "${BLUE}[***WordPress is already installed***]${RESET}"
+else
+	#download wordpress
+	su -s /bin/sh www-data  -c 'wp core download \
+		--allow-root \
+		--path="${WP_PATH}"'
 
-#download wordpress
-su -s /bin/sh www-data  -c 'wp core download --allow-root --path="/var/www/html/maustel.42.fr/public_html"'
+	# configure (create config file wp-config.php)
+	#TODO maybe only wp config create without su stuff
+	su -s /bin/sh www-data -c 'wp config create \
+		--path="$WP_PATH" \
+		--dbname="$MYSQL_DATABASE" \
+		--dbuser="$MYSQL_USER" \
+		--dbpass="$MYSQL_PASSWORD" \
+		--dbhost="$DB_HOST"\
+		--dbprefix=wp_ \
+		--allow-root'
 
-# configure
-su -s /bin/sh www-data -c 'wp config create --path="/var/www/html/maustel.42.fr/public_html" \
-	--dbname=wordpress \
-	--dbuser=wpuser \
-	--dbpass=password \
-	--dbhost=localhost \
-	--dbprefix=wp_ \
-	--allow-root'
+	# configure
+	# wp core config \
+	# 	--dbname=wp \
+	# 	--dbuser=wp_user \
+	# 	--dbpass=pw \
+	# 	--dbhost=localhost \
+	# 	--dbprefix=wp_ \
+	# 	--allow-root
 
-# configure
-# wp core config \
-# 	--dbname=wp \
-# 	--dbuser=wp_user \
-# 	--dbpass=pw \
-# 	--dbhost=localhost \
-# 	--dbprefix=wp_ \
-# 	--allow-root
+	#-------------------[Run WordPress] -----------
+	su -s /bin/sh www-data -c 'wp core install \
+		--path="$WP_PATH" \
+		--url=https://"$DOMAIN_NAME" \
+		--title=inception \
+		--admin_user="$WP_ADMIN" \
+		--admin_password="$WP_ADMIN_PASSWORD" \
+		--allow-root \
+		--admin_email=amsel@rainbow.com \
+		--skip-email'
 
-# -------------------[Wait for databse] -----------
-# wait for mariadb
-# todo
-# until wp db check --allow-root; do
-# 	sleep 5
-# done
+	# Create regular user
+	su -s /bin/sh www-data -c 'wp user create ${WP_USER} ${WP_USER}@${DOMAIN_NAME} \
+			--user_pass=${WP_USER_PASSWORD} \
+			--porcelain \
+			--allow-root'
+fi
 
-#-------------------[Run WordPress] -----------
-su -s /bin/sh www-data -c "wp core install \
-    --path=/var/www/html/maustel.42.fr/public_html \
-    --url=https://maustel.42.fr \
-    --title=inception \
-    --admin_user=admin \
-    --admin_password=pw \
-    --allow-root \
-    --admin_email=amsel@rainbow.com \
-	--skip-email"
+# Set WordPress to listen on port 9000
+# echo -e "${BLUE}[***Update PHP-FPM to listen on port 9000***]${RESET}"
+# sed -i 's|^listen = .*|listen = 9000|' /etc/php/7.4/fpm/pool.d/www.conf
 
-## Error: 'wp-config.php' not found.
-
+# Check if WordPress settings are correct
+# echo -e "${BLUE}[***Validating PHP-FPM configuration***]${RESET}"
+# php-fpm7.4 -t
 
 #-------------------[Execute] -----------
 echo "[END WP-SETUP-SCRIPT]"
